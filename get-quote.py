a5deddfe4367dc680a8b1a87f1f01e44f28d78fb
M Tamminen <21221663+mrainne@users.noreply.github.com>
2020-03-11 13:27:54 +0200

    Print two quotes, remove newlines from printing

--- get-quote.py
+++ get-quote.py
@@ -1,7 +1,7 @@
 import random
 
 def primary():
-  print("Keep it logically awesome.")
+  #print("Keep it logically awesome.")
 
   f = open("quotes.txt")
   quotes = f.readlines()
@@ -9,7 +9,7 @@ def primary():
 
   last = len(quotes)-1
   rnd = random.randint(0, last)
-  print(quotes[rnd])
-
+  print(quotes[rnd], end='')
+  print(quotes[random.randint(0, len(quotes)-1)], end='')
 if __name__== "__main__":
   primary()
